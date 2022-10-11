300185d22c6e89b330ef9f626a30a8fde73e2dbe
Alexey_Kovalenko_845 <kvlnk247@gmail.com>
2022-10-11 11:49:01 +0200

    Initial Commit

---
+++ Authentication block/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authentication block
+//
+//  Created by Алексей Коваленко on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
