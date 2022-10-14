50bb84aa287cc084d59e0eb485614932f90be4e0
K-Lesha <made.in.dn@gmail.com>
2022-10-14 11:00:28 +0200

    Appearance update test

--- Authentication block/Application/Appearance/Appearance.swift
+++ Authentication block/Application/Appearance/Appearance.swift
@@ -15,6 +15,7 @@ class Appearance {
     
     static func configure() {
 //        guard let titlesFont = UIFont(name: "Titles1", size: UIFont.labelFontSize) else { fatalError("failed") }
+        Appearance.titlesFont = UIFont.boldSystemFont(ofSize: 30)        Appearance.titlesFont = UIFont.boldSystemFont(ofSize: 30)
         Appearance.titlesFont = UIFont.boldSystemFont(ofSize: 30)
 //        guard let buttomsFont = UIFont(name: "Buttons", size: UIFont.buttonFontSize) else { fatalError("failed") }
         Appearance.buttomsFont = UIFont.boldSystemFont(ofSize: 12)
